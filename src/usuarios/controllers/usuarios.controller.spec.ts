81e30e9d805a12e5d55d545f66c147d6dbcf1d0e
cezartdev <cesaralonsosalazarp@gmail.com>
2024-12-31 17:11:41 -0300

    Se crea ambiente de prueba con SQLite y NestJS

--- src/usuarios/controllers/usuarios.controller.spec.ts
+++ src/usuarios/controllers/usuarios.controller.spec.ts
@@ -1,9 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+import { UsuariosModule } from '../usuarios.module';
+import { SequelizeModule, getModelToken } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { Usuarios } from '../../database/models/usuarios.model';
+import { Tipos } from '../../database/models/tipos.model';
 
 describe('UsuariosController (e2e)', () => {
-    const baseUrl = 'http://localhost:4000/api'; // Ajusta esto a la URL de tu servidor
+    let app: INestApplication;
+
     const token =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRlc2Fycm9sbG9AZ21haWwuY29tIiwibm9tYnJlIjoiRGVzYXJyb2xsbyIsImFwZWxsaWRvIjoiRGVzYXJyb2xsbyIsImNvbnRyYXNlw7FhIjoiJDJiJDEwJEQ0LzBTSUdjSDFjRUZLZUxJaWM0d2VjQWJUWWFGRlRMTk5YSlVBOTdqSnJrOExtcUhJSlJtIiwiZXN0YWRvIjoiQUNUSVZPIiwibm9tYnJlX3RpcG9zIjoiQURNSU5JU1RSQURPUiIsImNyZWF0ZWRBdCI6IjIwMjQtMTItMjlUMjE6NTA6MTAuMDAwWiIsInVwZGF0ZWRBdCI6IjIwMjQtMTItMjlUMjE6NTA6MTAuMDAwWiIsImlhdCI6MTczNTU5NjAwMiwiZXhwIjo0ODkxMzU2MDAyfQ.hGmV_Otp4dGDYsPy70NuPCcdmCA-F9Rp0uAUmFtgL6U';
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRlc2Fycm9sbG9AZ21haWwuY29tIiwibm9tYnJlIjoiRGVzYXJyb2xsbyIsImFwZWxsaWRvIjoiRGVzYXJyb2xsbyIsImNvbnRyYXNlw7FhIjoiJDJiJDEwJEQ0LzBTSUdjSDFjRUZLZUxJaWM0d2VjQWJUWWFGRlRMTk5YSlVBOTdqSnJrOExtcUhJSlJtIiwiZXN0YWRvIjoiQUNUSVZPIiwibm9tYnJlX3RpcG9zIjoiQURNSU5JU1RSQURPUiIsImNyZWF0ZWRBdCI6IjIwMjQtMTItMjlUMjE6NTA6MTAuMDAwWiIsInVwZGF0ZWRBdCI6IjIwMjQtMTItMjlUMjE6NTA6MTAuMDAwWiIsImlhdCI6MTczNTU5NjAwMiwiZXhwIjo0ODkxMzU2MDAyfQ.hGmV_Otp4dGDYsPy70NuPCcdmCA-F9Rp0uAUmFtgL6U'; // Tu token actual
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forRoot(),
+                SequelizeModule.forRoot({
+                    dialect: 'sqlite',
+                    storage: ':memory:',
+                    models: [Usuarios, Tipos], // Registra el modelo
+                    logging: false,
+                    autoLoadModels: true,
+                    synchronize: true,
+                }),
+                SequelizeModule.forFeature([Tipos]), // Importante: registramos el modelo
+                UsuariosModule,
+            ],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+        // Crear datos necesarios antes de las pruebas
+        const tiposModel = app.get(getModelToken(Tipos));
+        await tiposModel.create({
+            nombre: 'ADMINISTRADOR',
+            // Incluye aquí otros campos requeridos para el modelo Tipos
+        });
+    });
 
     it('debería crear un usuario', async () => {
         const crearUsuarioDto = {
@@ -15,14 +50,17 @@ describe('UsuariosController (e2e)', () => {
             nombre_tipos: 'ADMINISTRADOR',
         };
 
-        const res = await request(baseUrl)
+        return request(app.getHttpServer())
             .post('/usuarios/crear')
             .auth(token, { type: 'bearer' })
-            .send(crearUsuarioDto);
-
-        expect(res.status).toBe(201);
-        expect(res.body).toHaveProperty('email', 'test@test.com');
+            .send(crearUsuarioDto)
+            .expect(201)
+            .expect((res) => {
+                expect(res.body).toHaveProperty('email', 'test@test.com');
+            });
     });
 
-
+    afterAll(async () => {
+        await app.close();
+    });
 });

675fb5627db6faed023d00edb30d4a37492d18d3
cezartdev <cesaralonsosalazarp@gmail.com>
2025-01-03 19:11:45 -0300

    Se termina con la parte de geografia

--- src/geografia/controllers/regiones.controller.ts
+++ src/geografia/controllers/regiones.controller.ts
@@ -1,30 +1,28 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { RegionesService } from '../services/regiones.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ApiRespuestaError } from '../../common/utils/decorators';
-import { Regiones } from '../../database/models/regiones.model';
-import { BaseControllersSimple } from '../../common/base/base-controllers-simple-class';
+import { BaseControllersSimple } from '../../common/base/base-controllers-simple.class';
+import { RegionesService } from '../services/regiones.service';
 import { ObtenerPorIdRegionesDto } from '../dtos/regiones.dto';
 
+@ApiTags('Regiones')
 @Controller('regiones')
 export class RegionesController extends BaseControllersSimple {
     constructor(private readonly regionesService: RegionesService) {
         super();
     }
-    
+
     @ApiOperation({ summary: 'Obtener a todos las regiones' })
     @ApiRespuestaError()
     @Get('obtener-todos')
-    obtenerTodos(): Promise<Regiones[]> {
+    obtenerTodos() {
         return this.regionesService.obtenerTodos();
     }
 
     @ApiOperation({ summary: 'Obtener una region segun su clave primaria' })
     @ApiRespuestaError()
     @Get('obtener-por-id/:id')
-    obtenerPorId(@Param() id: ObtenerPorIdRegionesDto) {
-        return this.regionesService.obtenerPorId(id);
+    obtenerPorId(@Param() clavePrimaria: ObtenerPorIdRegionesDto) {
+        return this.regionesService.obtenerPorId(clavePrimaria);
     }
-
-
 }
--- src/geografia/services/provincias.service.ts
+++ src/geografia/services/provincias.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Provincias } from '../../database/models/provincias.model';
+import { BaseServicesSimple } from 'src/common/base/base-services-simple.class';
+import { ObtenerPorIdProvinciasDto } from '../dtos/provincias.dto';
 @Injectable()
-export class ProvinciasService {}
+export class ProvinciasService extends BaseServicesSimple {
+    async obtenerTodos(): Promise<Provincias[]> {
+        return await Provincias.findAll();
+    }
+
+    async obtenerPorId(
+        clavePrimaria: ObtenerPorIdProvinciasDto,
+    ): Promise<Provincias> {
+        const provincia = await Provincias.findByPk(clavePrimaria.id);
+
+        if (!provincia) {
+            throw new NotFoundException([
+                `No se encontró la provincia con id ${clavePrimaria.id}`,
+            ]);
+        }
+
+        return provincia;
+    }
+}
